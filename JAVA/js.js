bebe357448cbde8ed2d9b403b6e16b68f8bdb2ea
DanGarGR <hongosgamesv@gmail.com>
2024-08-14 18:50:25 -0600

    Update js.js

--- JAVA/js.js
+++ JAVA/js.js
@@ -10,7 +10,7 @@ function mostrarFormulario() {
 
     switch(figura) {
         case 'circulo':
-            imagenFigura.src = 'Figuras/Cuadrado.png';
+            imagenFigura.src = 'Figuras/Circulo.png';
             inputs.innerHTML = '<label for="radio">Radio:</label><input type="number" id="radio" placeholder="Radio">';
             break;
         case 'cuadrado':
